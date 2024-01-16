4c0647bcf59fe5d91b7049eb10a8e98b330764c6
sajadsalimzadeh <salimzadehsajad@gmail.com>
2024-01-16 17:46:52 +0330

    feature: implement job service and first test of import task

--- Hosts/SeedHost.cs
+++ Hosts/SeedHost.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Dorbit.Framework.Attributes;
+using Dorbit.Framework.Entities.Abstractions;
 using Dorbit.Framework.Hosts;
 using Dorbit.Framework.Utils.Json;
 using Dorbit.Identity.Entities;
@@ -28,25 +32,12 @@ public class SeedHost : BaseHost
         var userRepository = sp.GetService<UserRepository>();
         var accessRepository = sp.GetService<AccessRepository>();
 
-        var allAccesses = await accessRepository.Set().Include(x => x.Parent).ToListAsync();
-
-        async Task ImportAsync(ICollection<Access> accesses, Access parent)
-        {
-            foreach (var access in accesses)
-            {
-                if (allAccesses.Any(x => x.Name == access.Name && x.Parent?.Name == access.Parent.Name)) continue;
-                var children = access.Children;
-                access.ParentId = parent?.Id;
-                access.Children = null;
-                var entity = await accessRepository.InsertAsync(access);
-                if (children is not null) await ImportAsync(children, entity);
-            }
-        }
+        var allAccesses = await accessRepository.Set().Include(x => x.Parent).ToListAsync(cancellationToken: cancellationToken);
 
         var accessFilename = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets/accesses.json");
         if (File.Exists(accessFilename))
         {
-            var json = await File.ReadAllTextAsync(accessFilename);
+            var json = await File.ReadAllTextAsync(accessFilename, cancellationToken);
             var accesses = JsonSerializer.Deserialize<List<Access>>(json);
             await ImportAsync(accesses, null);
         }
@@ -55,7 +46,7 @@ public class SeedHost : BaseHost
         var userService = sp.GetService<UserService>();
         if (string.IsNullOrEmpty(AppIdentity.Setting.Admin.Username)) return;
 
-        if (!await userRepository.Set().AnyAsync(x => x.Username.ToLower() == "admin"))
+        if (!await userRepository.Set().AnyAsync(x => x.Username.ToLower() == "admin", cancellationToken: cancellationToken))
         {
             await userService.AddAsync(new UserAddRequest()
             {
@@ -67,6 +58,21 @@ public class SeedHost : BaseHost
                 Email = AppIdentity.Setting.Admin.Email,
                 NeedResetPassword = true
             });
+        }
+
+        return;
+
+        async Task ImportAsync(IEnumerable<Access> accesses, IEntity parent)
+        {
+            foreach (var access in accesses)
+            {
+                if (allAccesses.Any(x => x.Name == access.Name && x.Parent?.Name == access.Parent.Name)) continue;
+                var children = access.Children;
+                access.ParentId = parent?.Id;
+                access.Children = null;
+                var entity = await accessRepository.InsertAsync(access);
+                if (children is not null) await ImportAsync(children, entity);
+            }
         }
     }
 }
