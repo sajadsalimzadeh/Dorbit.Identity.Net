71179ac067aa486b09e63bef2ed77e40a03ccacd
sajad <salimzadehsajad@gmail.com>
2024-04-21 20:18:19 +0330

    otp code hash migration

---
+++ Migrations/20240421164717_IncreaseCodeHashOfOtp.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dorbit.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseCodeHashOfOtp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CodeHash",
+                schema: "identity",
+                table: "Otp",
+                type: "character varying(64)",
+                maxLength: 64,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(32)",
+                oldMaxLength: 32,
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Otp_CodeHash",
+                schema: "identity",
+                table: "Otp",
+                column: "CodeHash");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Otp_CodeHash",
+                schema: "identity",
+                table: "Otp");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodeHash",
+                schema: "identity",
+                table: "Otp",
+                type: "character varying(32)",
+                maxLength: 32,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(64)",
+                oldMaxLength: 64,
+                oldNullable: true);
+        }
+    }
+}
