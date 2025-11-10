d2e99313c21cd466e1aa70c25a25b6052068d37d
sajad salimzadeh <salimzadehsajad@gmail.com>
2025-11-10 17:52:29 +0330

    improve: add getAll otp api

--- Controllers/OtpsController.cs
+++ Controllers/OtpsController.cs
@@ -1,15 +1,25 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Dorbit.Framework.Contracts.Results;
 using Dorbit.Framework.Controllers;
+using Dorbit.Framework.Extensions;
 using Dorbit.Identity.Contracts.Auth;
+using Dorbit.Identity.Entities;
+using Dorbit.Identity.Repositories;
 using Dorbit.Identity.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dorbit.Identity.Controllers;
 
 [Route("Identity/[controller]")]
-public class OtpsController(OtpService otpService) : BaseController
+public class OtpsController(OtpRepository otpRepository, OtpService otpService) : BaseController
 {
+    public Task<QueryResult<List<Otp>>> GetAllAsync()
+    {
+        return otpRepository.Set().ToListAsync().ToQueryResultAsync();
+    }
+    
     [HttpPost]
     public async Task<CommandResult> SendAsync([FromBody] AuthSendOtpRequest request)
     {
