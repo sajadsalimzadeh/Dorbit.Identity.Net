acb9a66f5f2f73103dea83a48dd64f0b3c04fa9e
sajad <salimzadehsajad@gmail.com>
2026-02-04 22:07:22 +0330

    improve: add default username configuration admin user

--- Configs/ConfigAdmin.cs
+++ Configs/ConfigAdmin.cs
@@ -3,7 +3,7 @@ namespace Dorbit.Identity.Configs;
 public class ConfigAdmin
 {
     public string Name { get; set; }
-    public string Username { get; set; }
+    public string Username { get; set; } = "admin";
     public string Password { get; set; }
     public string Email { get; set; }
     public string Cellphone { get; set; }
